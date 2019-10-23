47e4ae51ac1e80b1861004759bc988f5e83dbc01
kuroharu0430 <kuroharu0430@gmail.com>
2019-10-23 20:52:26 +0900

    commit

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,16 +1,4 @@
 $(function(){
-  
-<<<<<<< Updated upstream
-  $(document).on("click",".show__bar-image",function(){
-    var show_index = $(this).attr("data-index")
-    console.log(show_index)
-    $('.show_image').fadeOut()
-    $(`.show_image[data-index = ${show_index}]`).fadeIn(1000)
-    $('.show__bar-image').css("opacity","1")
-    $(`.show__bar-image[data-index = ${show_index}]`).css("opacity","0.5")
-  })
-
-=======
   j = 0
   $(".works").on("click",".show__bar-image",function(){
     j = $(this).attr("data-index")
@@ -21,6 +9,4 @@ $(function(){
   })
 
 
-
->>>>>>> Stashed changes
 });
