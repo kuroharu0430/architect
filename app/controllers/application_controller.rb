449f13204d26ead44a00af7f58aa4a234e545b92
kuroharu0430 <kuroharu0430@gmail.com>
2019-10-29 18:14:57 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,19 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
   end
 
-  def after_sign_up_path_for(resource)
-    customer_messages_path(current_customer)
-  end
-
   def after_sign_in_path_for(resource)
-    customer_messages_path(current_customer)
+    case resource
+    when Customer
+      customer_messages_path(current_customer)
+    when AdminUser
+      #AdminUser（管理ユーザ）がログインしたときの処理
+      stored_location_for(resource) ||
+       if resource.is_a?(AdminUser)
+         root_path
+       else
+         super
+       end
+    end
   end
   
 end
