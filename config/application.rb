6e84b053f677e92ac1decd9908dd7cc59a070709
kuroharu0430 <kuroharu0430@gmail.com>
2019-10-31 23:45:28 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module Architect
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.i18n.default_locale = :ja
-    config.time_zone = 'Osaka'
+    # config.i18n.default_locale = :ja
+    # config.time_zone = 'Osaka'
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
