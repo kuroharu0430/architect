25866674aef75bb65219c07eafdeeb166f4de960
kuroharu0430 <kuroharu0430@gmail.com>
2019-10-31 23:48:24 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module Architect
       g.javascripts false
       g.helper false
       g.test_framework false
+      config.i18n.default_locale = :ja
+      config.time_zone = 'Osaka'
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
