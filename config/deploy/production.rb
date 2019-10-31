93a8f9d1e38f694ae60f2d3e396aa2c6ab6d1cc8
kuroharu0430 <kuroharu0430@gmail.com>
2019-10-31 18:08:03 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '13.113.167.56', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -49,9 +51,9 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "13.113.167.56/",
-#   user: "ec2-user"
-#   roles: %w{app db web},
+# server "example.com",
+#   user: "user_name",
+#   roles: %w{web app},
 #   ssh_options: {
 #     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
