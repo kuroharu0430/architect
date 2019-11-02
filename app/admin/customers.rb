5d92869d2420ffcd7c70d3ef5c6afc66d4fb68f6
kuroharu0430 <kuroharu0430@gmail.com>
2019-11-03 01:13:20 +0900

    admin　仮完成

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,4 +1,20 @@
 ActiveAdmin.register Customer do
+  permit_params :state
+
+  config.clear_action_items!
+
+  action_item :my_button1, only: :index do
+    link_to "TOP画面へ", root_path
+  end
+
+
+  form do |f|
+    f.inputs '登録' do
+      f.input :state
+      end
+      f.actions
+  end
+
   index do
     column :id
     column :name
@@ -8,7 +24,7 @@ ActiveAdmin.register Customer do
     actions defaults: true do |customer|
       item ('message'), customer_messages_path(customer), class: 'view_link member_link'
     end
-    
+
   end
 
   # See permitted parameters documentation:
--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,4 +1,33 @@
 ActiveAdmin.register Product do
+  config.clear_action_items!
+
+  action_item :my_button1, only: :index do
+    link_to "Product new", new_product_path
+  end
+  action_item :my_button1, only: :index do
+    link_to "TOP画面へ", root_path
+  end
+
+
+  index defaults: true do
+    column :name
+    column :text
+    column :image
+    column :date
+    column :place
+    column :purpose
+    column :scale
+    column :site_area
+    column :building_area
+    column :architectural_area
+
+  actions defaults: false do |product|
+      item ('edit or delete'), edit_product_path(product), class: 'view_link member_link'
+      # item ('delete'), product_path(product) ,method: delete, class: 'view_link member_link'
+    end
+
+  end
+
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -14,5 +43,6 @@ ActiveAdmin.register Product do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
   
 end
