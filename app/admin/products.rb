5f062b511e6e351da51e9a33c0fcbd2c48c2a39b
kuroharu0430 <kuroharu0430@gmail.com>
2019-11-02 16:04:13 +0900

    as

---
+++ app/admin/products.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Product do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :text, :image, :date, :place, :purpose, :scale, :site_area, :building_area, :architectural_area
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :text, :image, :date, :place, :purpose, :scale, :site_area, :building_area, :architectural_area]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
