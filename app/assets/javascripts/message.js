084ecf7c7df96570529c93f17dd5281b87b6132d
kuroharu0430 <kuroharu0430@gmail.com>
2019-11-03 16:46:05 +0900

    message scroll

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,4 @@
 // $(function(){
-  
 //   $('#messages-form-btn').click(function(){
 //     if ($('.messages-form-text').val() == '') {
 //       // console.log("dada")
@@ -8,9 +7,16 @@
 //     $(document).on('ajax:success', '#message_form', function(e) {
 //       console.log(e)
 //       $('.messages-form-text').val('');
-
 //       $('.messages-main__chat').prepend('<p>' + e.detail[0] + '</p>');
 //     })
   
 //   })
 // });
+
+$(function(){
+  if (window.location.href.match(/\/customers\/\d+\/messages/)){
+    var last_message_height =$('.messages-main').get(0).scrollHeight
+    $('.messages-main').animate({scrollTop:last_message_height},10);
+
+  }
+})
