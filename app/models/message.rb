f68e42d90a929d6fb890fd27aadaa2f188e09312
kuroharu0430 <kuroharu0430@gmail.com>
2019-11-04 02:44:50 +0900

    文字　修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ApplicationRecord
   belongs_to :customer
   mount_uploader :image, ImageUploader
 
-  validates :text, presence: true, length: {maximum: 40}
+  validates :text, presence: true, length: {maximum: 300}
 end
