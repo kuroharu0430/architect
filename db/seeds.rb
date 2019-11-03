6f4d0b1b6e655bcf013b1d2c8d656c38ff7d2a57
kuroharu0430 <kuroharu0430@gmail.com>
2019-11-03 15:04:45 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'merryjun@msn.com', password: 'hanage0614', password_confirmation: 'hanage0614')
-# AdminUser.create!(email: 'kuroharu0430@gmail.com', password: 'sazanka3033', password_confirmation: 'sazanka3033')
+AdminUser.create!(email: 'kuroharu0430@gmail.com', password: 'sazanka3033', password_confirmation: 'sazanka3033')
 
-# Customer.create!(name:'奥野 潤',  email: 'merryjun@msn.com', password: 'hanage0614', password_confirmation: 'hanage0614')
-# Customer.create!(name:'kuroharu',email: 'kuroharu0430@gmail.com', password: 'sazanka3033', password_confirmation: 'sazanka3033')
-# Customer.create!(name:'test user',email: 'test@gmail.com', password: '123456', password_confirmation: '123456')
+Customer.create!(name:'奥野 潤',  email: 'merryjun@msn.com', password: 'hanage0614', password_confirmation: 'hanage0614')
+Customer.create!(name:'kuroharu',email: 'kuroharu0430@gmail.com', password: 'sazanka3033', password_confirmation: 'sazanka3033')
+Customer.create!(name:'test user',email: 'test@gmail.com', password: '123456', password_confirmation: '123456')
