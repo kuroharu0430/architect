fd1ec3306848b25fcbfcb502119f772041197a1d
kuroharu0430 <kuroharu0430@gmail.com>
2019-11-06 22:31:34 +0900

    add

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,9 +1,11 @@
 $(function(){
     $('.nav__name').addClass('move')
+    $('.top__left-message, .top__card-letter, .header__list' ).css("display","none")
     $('.top').animate({'opacity':"1"},3000,function(){
       $('.reflection').fadeIn() 
       $('.top').addClass('brightly')
       $('.top__message').fadeIn()
+      $('.top__left-message, .top__card-letter, .header__list' ).show("drop",{direction:"down"},1000)
 
 
     })
