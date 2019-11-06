76c03b68598d57d9df09d220e75451251c1c7193
kuroharu0430 <kuroharu0430@gmail.com>
2019-11-06 21:50:34 +0900

    top　view変更

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,5 +1,13 @@
 $(function(){
-    $('.mask').addClass('add_color')
-    $('.reflection').fadeIn() 
+    $('.nav__name').addClass('move')
+    $('.top').animate({'opacity':"1"},3000,function(){
+      $('.reflection').fadeIn() 
+      $('.top').addClass('brightly')
       $('.top__message').fadeIn()
+
+
+    })
+
+
+
 })
