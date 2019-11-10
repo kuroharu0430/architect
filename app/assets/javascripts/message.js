e52e92739ddb9c403c8586a8cf7109e14a335d27
kuroharu0430 <kuroharu0430@gmail.com>
2019-11-10 22:42:08 +0900

    message自動更新テスト

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,13 +4,16 @@ $(function(){
     $('.messages-main').animate({scrollTop:last_message_height},10);
 
     var reload_messages = function(){
-      var last_message_id = $('.message:last').attr("data-mesage_id")
-      $.ajax({
-        url:'messages/auto',
-        type:'get',
-        datatype:'json',
-        data:{id: last_message_id} 
-      })
+      if (last_message_id < $('.message:last').attr("data-mesage_id")){
+        var last_message_id = $('.message:last').attr("data-mesage_id")
+
+        $.ajax({
+          url:'messages/auto',
+          type:'get',
+          datatype:'json',
+          data:{id: last_message_id} 
+        })
+      } 
     }
   }
   setInterval(reload_messages, 5000)
