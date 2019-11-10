8a2ada773dc68b582bdc51c09b96ef916545978a
kuroharu0430 <kuroharu0430@gmail.com>
2019-11-11 01:14:15 +0900

    test

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,18 +1,22 @@
 $(function(){
+  l=''
   if (window.location.href.match(/\/customers\/\d+\/messages/)){
     var last_message_height =$('.messages-main').get(0).scrollHeight
     $('.messages-main').animate({scrollTop:last_message_height},10);
 
     var reload_messages = function(){
-      if (last_message_id < $('.message:last').attr("data-mesage_id")){
-        var last_message_id = $('.message:last').attr("data-mesage_id")
+      console.log(l)
+    console.log($('.message:last').attr("data-mesage_id"))
+      var last_message_id = $('.message:last').attr("data-mesage_id")
+      if (last_message_id > l){
+        l = last_message_id
         $.ajax({
           url:'messages/auto',
           type:'get',
           datatype:'json',
           data:{id: last_message_id} 
         })
-      } 
+      }
     }
   }
   setInterval(reload_messages, 5000)
